266b0718da469d0ce819309e3d7963ea0b457ac9
valerio <valerio.deprosperi@gmail.com>
2020-09-22 16:02:12 +0200

    template handlebars

--- js/script.js
+++ js/script.js
@@ -5,15 +5,15 @@ $(document).ready(function(){
   $.ajax(
     {
 
-      "url": "https://api.themoviedb.org/3/search/movie"
+      "url": "https://api.themoviedb.org/3/search/movie",
       "data": {
         "api_key":"998db081c3b816179a3b215856e82b99",
         "query": searchMovie,
         "language": "it-IT"
       },
-      "method": "GET"
+      "method": "GET",
       "success": function(data){
-        console.log(data);
+        console.log(data.results);
       },
       "error": function(err){
         alert("Errore!");
@@ -28,3 +28,7 @@ $(document).ready(function(){
 
 
 });
+
+function renderMovie(movies){
+  console.log(movies);
+}
