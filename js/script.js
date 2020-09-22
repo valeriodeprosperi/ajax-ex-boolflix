47448cab4dc466fc40122956184b8ac0ae8aa574
valerio <valerio.deprosperi@gmail.com>
2020-09-22 15:36:49 +0200

    chiamata ajax

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 $(document).ready(function(){
 
+  var searchMovie = "Il Padrino";
+
   $.ajax(
     {
 
       "url": "https://api.themoviedb.org/3/search/movie"
+      "data": {
+        "api_key":"998db081c3b816179a3b215856e82b99",
+        "query": searchMovie,
+        "language": "it-IT"
+      },
+      "method": "GET"
+      "success": function(data){
+        console.log(data);
+      },
+      "error": function(err){
+        alert("Errore!");
+      }
     }
 
 
